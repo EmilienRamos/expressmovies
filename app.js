54f6d17eae936cef223d540e04c1e3c485477c3d
Emilien Ramos <eramos@digitalvirgo.com>
2021-05-31 12:20:54 +0200

    Section 2 "Routage in details"

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+const PORT = 3000;
+
+app.use('/public', express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/movies', (req, res) => {
+  res.send('Movies');
+});
 
+app.get('/movies/add', (req, res) => {
+  res.send();
+});
+
+app.get('/movies/:id', (req, res) => {
+  res.send('Movies ' + req.params.id);
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
